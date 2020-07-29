26dae8bb9c9c22a6ebc726d0c270cb1ddd5359f5
Tóth Bence <toth.bence@encosoft.hu>
2020-07-29 15:42:29 +0200

    read-write serial port

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const { ipcMain } = require('electron');
+const SerialPort = require("serialport");
+const port = new SerialPort("COM4", { baudRate: 9600 });
+const ipcRenderer = require('electron').ipcRenderer;
 
 function createWindow () {
   // Create the browser window.
@@ -8,36 +12,52 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      webSecurity: false,
+      plugins: true,
+      nodeIntegration: true
     }
-  })
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
+  // mainWindow.webContents.openDevTools()
+
+  port.open(() => {
+    console.log('Connected');
+  });
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
 
+
+ipcMain.on("serial-port-write", function (event, arg) {
+  port.write(arg);
+  event.sender.send("new-line", arg + ' sent');
+  port.on('data', line => event.sender.send("new-line", "> " + line))
+});
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
   
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
-})
+});
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
